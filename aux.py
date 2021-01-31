4f9dd32eafa33f57d71c1aec51971e25f8d2eeeb
Dominic <foldvari@mcule.com>
2021-01-31 17:54:34 +0100

    more aux functions

--- aux.py
+++ aux.py
@@ -64,7 +64,7 @@ def flip_at(iterator, indices):
     return ret
 
 
-def enum_quotient_graph(dimensions, alphabet_length=4):
+def enum_quotient_graph_length(dimensions, alphabet_length=4):
     length = len(dimensions)
     node_levels = []
     for flip in range(length+1):
@@ -82,3 +82,54 @@ def increase_dimension(characters, increase=4):
     for jdx in range(increase):
         blocks = TS.gen_hamming_graph(taboos, idx+1+jdx)
     return
+
+
+def enum_quotient_graph(characters, skip=1):
+    not_allowed = [letters-pos for pos in characters]
+    length = len(characters)
+    node_groups = []
+    index_groups = []
+    jdx = 0
+    for idx in range(length+1):
+        if idx == skip:
+            continue
+        index_groups.append([])
+        for indices in combinations(range(length),idx):
+            jdx += 1
+            index_groups[idx].append(jdx)
+            current_characters = [c for c in characters]
+            for i in indices:
+                current_characters[i] = not_allowed[i]
+            node_groups.append((jdx,current_characters))
+    index_groups = [(1,), tuple(range(2,jdx+1))]
+    return node_groups, index_groups
+
+
+def extend_left_and_right(node_groups):
+    node_groups_right = []
+    node_groups_left = []
+    for group in node_groups:
+        node_groups_left.append((group[0], [letters] + group[1]))
+        node_groups_right.append((group[0], group[1] + [letters]))
+    return node_groups_left, node_groups_right
+
+
+def overlaps(node_groups, index_groups):
+    left, right = extend_left_and_right(node_groups)
+    new_node_groups =  []
+    for p1,p2 in direct_product([left, right]):
+        ints = [s1.intersection(s2) for s1, s2 in zip(p1[1],p2[1])]
+        if all(ints):
+            new_node_groups.append(((p1[0], p2[0]), ints))
+    indices = [n[0] for n in new_node_groups]
+    new_index_groups = [] #TODO
+    return new_node_groups, new_index_groups
+
+
+def inspect_dimension_increment_in_quotient_graph(characters, inc=1):
+    start_node_groups, start_index_groups = enum_quotient_graph(characters)
+    node_groups, index_groups = overlaps(start_node_groups, start_index_groups)
+    for s in start_node_groups:
+        logger.info(s)
+    for n in node_groups:
+        logger.info(n)
